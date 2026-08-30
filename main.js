dabd680b52e7957543c42e3a6d89c4861d5937e8
Outlive Studio <outlivestudiodev@gmail.com>
2026-08-30 04:01:42 +0100

    Added win condition and win message

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 0;
+let gameRunning = true;
 
 const playerScoreDOM = document.getElementById("player-score");
 const computerScoreDOM = document.getElementById("computer-score");
@@ -30,16 +31,17 @@ function getComputerChoice() {
 
 function updateScoresDOM() {
     playerScoreDOM.innerHTML = `Your Score <br>${playerScore}`;
-    computerScoreDOM.innerHTML = `Your Score <br>${computerScore}`;
+    computerScoreDOM.innerHTML = `Computer Score <br>${computerScore}`;
 }
 
 function playRound(humanChoice, computerChoice) {
-    roundNumber++;
+    if (!gameRunning) {return}
 
     console.log(`Player picked ${humanChoice}, Computer picked ${computerChoice}.`)
     let winner;
 
     if (humanChoice != computerChoice) {
+        roundNumber++; // We only increment round if its not a draw.
         if ( (humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper") ) {
             winner = "Human";
             playerScore++;
@@ -57,6 +59,12 @@ function playRound(humanChoice, computerChoice) {
         gameMessageDOM.innerText = `Player picked ${humanChoice}, Computer picked ${computerChoice}. ${winner} gets 1 point!`;
     } else {
         gameMessageDOM.innerText = `Player picked ${humanChoice}, Computer picked ${computerChoice}. It's a draw!`;
+    }
+
+    if (roundNumber == 5) {
+        gameRunning = false;
+        let gameWinner = playerScore > computerScore && "You win!" || "Computer wins!";
+        gameMessageDOM.innerText = `Game over! ${gameWinner}`
     }
 }
 
