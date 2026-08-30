0042c79a3d5cf8c592b8965372d9ebcb198a1d25
Outlive Studio <outlivestudiodev@gmail.com>
2026-08-30 03:53:39 +0100

    Added play round logic and html score updates

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+
+const playerScoreDOM = document.getElementById("player-score");
+const computerScoreDOM = document.getElementById("computer-score");
+
+
 function getComputerChoice() {
     let numberChosen = Math.floor(Math.random() * 3);
     let choice;
@@ -19,8 +27,43 @@ function getComputerChoice() {
     return choice;
 }
 
+function updateScoresDOM() {
+    playerScoreDOM.innerHTML = `Your Score <br>${playerScore}`;
+    computerScoreDOM.innerHTML = `Your Score <br>${computerScore}`;
+}
+
+function playRound(humanChoice, computerChoice) {
+    roundNumber++;
+
+    console.log(`Player picked ${humanChoice}, Computer picked ${computerChoice}.`)
+    let winner;
+
+    if (humanChoice != computerChoice) {
+        if ( (humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper") ) {
+            winner = "Human";
+            playerScore++;
+        }   else {
+            winner = "Computer";
+            computerScore++;
+        }
+    } else {
+        winner = "draw";
+    }
+
+    updateScoresDOM();
+
+    if (winner != "draw"){
+        let winnerMessage = `Player picked ${humanChoice}, Computer picked ${computerChoice}. ${winner} gets 1 point!`;
+        console.log(winnerMessage);
+    } else {
+        let winnerMessage = `Player picked ${humanChoice}, Computer picked ${computerChoice}. It's a draw!`;
+        console.log(winnerMessage);
+    }
+}
+
 document.querySelectorAll("button").forEach((button) => {
     button.addEventListener("click", (event) => {
-        console.log("SELECTED OPTION", button.id)
+        humanChoice = button.id;
+        playRound(humanChoice, getComputerChoice());
     });
 });
